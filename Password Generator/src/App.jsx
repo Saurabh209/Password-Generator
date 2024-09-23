bee0d9370c504a9778adbaad90906f29b20a717d
Saurabh209 <saurabhhh2613@gmail.com>
2024-09-23 20:53:47 +0530

    basic setup done

---
+++ Password Generator/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [Password, setPassword] = useState(0)
+  let [Num, setNum] = useState(false)
+  let [Char, setChar] = useState(false)
+  let [length, setlength] = useState(8)
+
+  return (
+    <>
+      <div class="container">
+
+      </div>
+    </>
+  )
+}
+
+export default App
